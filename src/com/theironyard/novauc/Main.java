6feceacbdc10db12dc16c615e228e9fa8e1818c5
Steve Jobs <peter@steves-mbp.nvstu.edu>
2017-03-01 16:51:56 -0500

    commit 2

--- src/com/theironyard/novauc/Main.java
+++ src/com/theironyard/novauc/Main.java
@@ -1,8 +1,39 @@
 package com.theironyard.novauc;
 
+import spark.ModelAndView;
+import spark.Session;
+import spark.Spark;
+
+import java.util.HashMap;
+
 public class Main {
 
+    public static HashMap<String, User> globalGps = new HashMap<>();
+
     public static void main(String[] args) {
+
+        Spark.init();
+
+        Spark.get("/", (((request, response) -> {
+            Session saison = request.session();
+            String nombre = saison.attribute("userName");
+            User user = globalGps.get(nombre);
+
+            HashMap localGps = new HashMap();
+            if (user == null) {
+                return new ModelAndView(localGps, "index.html");
+            }
+            else {
+                return new ModelAndView(user, "messages.html");
+            }
+
+                }))
+        );
+
+        //Spark.post("/create-user");
+        //Spark.post("/create-message");
+        //Spark.post("/delete-message");
+        //Spark.post("/edit-message");
 
     }
 }
---
+++ src/com/theironyard/novauc/User.java
@@ -0,0 +1,14 @@
+package com.theironyard.novauc;
+
+
+import java.util.ArrayList;
+
+public class User {
+    private String nombre;
+    //private ArrayList<Message> message = new ArrayList<>();
+
+    public User(String nombre, ArrayList message) {
+        this.nombre = nombre;
+        //this.message = message;
+    }
+}
