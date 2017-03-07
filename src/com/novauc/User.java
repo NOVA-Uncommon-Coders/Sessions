df9c02f81de0d2b8e06364bd802d151d75e8f5bc
Octavio Cisneros <octavio@octavios-air.nvstu.edu>
2017-03-06 19:16:28 -0500

    Progress is good

---
+++ src/com/novauc/User.java
@@ -0,0 +1,21 @@
+package com.novauc;
+
+import java.util.ArrayList;
+
+
+
+public class User {
+    String name;
+    String password;
+    String ymessage;
+ //   ArrayList<Message> messages  = new ArrayList<>();
+
+    public User(String name, String password, String ymessage) {
+        this.name = name;
+        this.password = password;
+        this.ymessage = ymessage;
+        //   this.message = message;
+
+    }
+}
+
